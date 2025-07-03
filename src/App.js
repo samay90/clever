efba1bf8e2f93fab2561d128e45030a2af812581
samay90 <samaypatel90@gmail.com>
2025-07-03 17:46:41 +0530

    Routing issue solved

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Settings from './pages/Settings'
 import SettingsProfile from './pages/SettingsProfile'
 import FileViewer from './components/FileViewer'
 const App = () => {
-  const url = "http://192.168.0.102:9920"
+  const url = "https://clever-alpha.vercel.app"
   const [token,setToken] = React.useState("")
   const navigate = useNavigate()
   const location = useLocation()
@@ -90,6 +90,9 @@ const App = () => {
       getUser()
     }
   },[token])
+  useEffect(() => {
+    if (location.pathname==="/"){navigate("/app/home")}
+  },[location.pathname])
   return (
     <> 
       <Toaster/>
